5d2228c5b48248c13200d44b01e075d83e3b8e3f
Joe Bothwell <jobothw@microsoft.com>
2020-07-29 13:23:42 -0400

    Ver1

---
+++ DeployTemplate.ps1
@@ -0,0 +1,7 @@
+﻿Connect-AzAccount -Environment AzureUSGovernment
+
+$templateFile = "C:\Templates\bastion.json"
+New-AzResourceGroupDeployment `
+ -Name BastionTemplate `
+ -ResourceGroupName jb-rg `
+ -TemplateFile $templateFile
