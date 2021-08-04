370080e04be34008eba6c4c094a85265d321921b
Joe Bothwell <jobothw@microsoft.com>
2021-08-04 15:29:34 -0400

    Added additional parameters

--- DeployTemplate.ps1
+++ DeployTemplate.ps1
@@ -1,8 +1,8 @@
 ﻿Connect-AzAccount -Environment AzureUSGovernment
 emp\$templateFile = "C:\Templates\Templates\bastion.json"
-$parameterFile = "C:\Templates\Templates\parameters.josn"
+$parameterFile = "C:\Repro\Templates\Bastion.parameters.josn"
 New-AzResourceGroupDeployment `
  -Name BastionTemplate `
  -ResourceGroupName jb-rg `
  -TemplateFile $templateFile `
- -TemplateParameterFile $parameters
+ -TemplateParameterFile $parameterFile
